2671c12d2504e2bc5d63661d838c9f4275e034fd
Cytilopsis <10235101424@stu.ecnu.edu.cn>
2026-04-22 18:05:41 +0800

    test pictures

---
+++ prepare_download.sh
@@ -0,0 +1,4 @@
+cp out/bearpi_hm_micro/bearpi_hm_micro/OHOS_Image.stm32 applications/BearPi/BearPi-HM_Micro/tools/download_img/kernel/OHOS_Image.stm32
+cp out/bearpi_hm_micro/bearpi_hm_micro/rootfs_vfat.img applications/BearPi/BearPi-HM_Micro/tools/download_img/kernel/rootfs_vfat.img
+cp out/bearpi_hm_micro/bearpi_hm_micro/userfs_vfat.img applications/BearPi/BearPi-HM_Micro/tools/download_img/kernel/userfs_vfat.img
+echo "Successfully copied OHOS_Image.stm32, rootfs_vfat.img and userfs_vfat.img to applications/BearPi/BearPi-HM_Micro/tools/download_img/kernel/"
